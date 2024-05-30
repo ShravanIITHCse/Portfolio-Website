eed4eebb8f6c87159cc42f8c83d85b3234a17b2a
Shravan Badgujar <cs22btech11010@iith.ac.in>
2024-05-30 21:18:05 +0530

    made navbar more responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import linkedin from '../assets/img/linkedin.svg';
 import github from '../assets/img/github.svg';
-import leetcode from '../assets/img/leetcode.svg';
-import codeforces from '../assets/img/code-forces.svg';
+import facebook from '../assets/img/facebook.svg';
+import instagram from '../assets/img/instagram.svg';
 
 export const Footer = () => {
     return (
@@ -19,11 +19,11 @@ export const Footer = () => {
 
                     </Col>
                     <Col sm={4} className='text-center text-sm-end'>
-                        <div className="social-icon">
+                        <div className="social-icon" id='footer-social-icon'>
                             <a href="https://www.linkedin.com/in/shravan-badgujar-a78201269/"><img src={linkedin} alt="Linkedin" /></a>
                             <a href="https://github.com/ShravanIITHCse"><img src={github} alt="Github" /></a>
-                            <a href="https://leetcode.com/u/Three_swords_zoro/"><img src={leetcode} alt="Leetcode" /></a>
-                            <a href="https://codeforces.com/profile/Shravanair160"><img src={codeforces} alt="CodeForces" /></a>
+                            <a href="https://www.instagram.com/the_shravan_badgujar/"><img src={instagram} alt="Instagram" /></a>
+                            <a href="https://www.facebook.com/profile.php?id=100084780055776"><img src={facebook} alt="Facebook" /></a>
                         </div>
                         <p>CopyRight 2024. All Right Reserved by Shravan Badgujar</p>
                     </Col>
